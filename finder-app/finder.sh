e4600075b30c762232d6c956dcae1d8cd532d510
DivyeshShashikant <divyesh.patel@colorado.edu>
2022-01-15 16:01:07 -0700

    Assignment 1 Final Code

--- finder-app/finder.sh
+++ finder-app/finder.sh
@@ -1,27 +1,37 @@
 #!/bin/bash
 
+# Author: Divyesh Patel
+# File: finder.sh
+
+# First argument is a path to directory	
+filesdir=$1
+
+# Second argument is a text string which is searched within directory
+searchstr=$2
+
+
 # Check for errors if arguments are invalid
 if [ ! $# -eq 2 ]
 then
-	echo Invalid Parameters
+	echo Invalid Parameters, $# entered 2 expected
 	exit 1
 
 # Check for errors if directory not found
-elif [ ! -d $1 ]
+elif [ ! -d $filesdir ]
 then
-	echo $1 Cannot be found
+	echo $filesdir Cannot be found
 	exit 1
 
 fi
 
-	echo pass
 
 # Number of files in the directory and all subdirectories
-FILECOUNT=$(find $1 -type f | wc -l)
+FILECOUNT=$(find $filesdir -type f | wc -l)
 
 # Number of matching lines found in respective files
-LINESCOUNT=$(grep -r $2 $1 | wc -l)
+LINESCOUNT=$(grep -r "$searchstr" $filesdir | wc -l)
 
 # print message
 echo The number of files are $FILECOUNT and the number of matching lines are $LINESCOUNT 
 
+exit 0
