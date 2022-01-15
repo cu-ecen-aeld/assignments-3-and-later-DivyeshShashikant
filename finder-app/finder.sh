2d7359a5f4ac6887d111f3ac27eea886f0cbbe7a
DivyeshShashikant <divyesh.patel@colorado.edu>
2022-01-15 02:11:57 -0700

    Comments added

--- finder-app/finder.sh
+++ finder-app/finder.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+# Check for errors if arguments are invalid
 if [ ! $# -eq 2 ]
 then
 	echo Invalid Parameters
 	exit 1
 
+# Check for errors if directory not found
 elif [ ! -d $1 ]
 then
 	echo $1 Cannot be found
@@ -13,7 +15,13 @@ then
 fi
 
 	echo pass
-	FILECOUNT=$(find $1 -type f | wc -l)
-	LINESCOUNT=$(grep -r $2 $1 | wc -l)
-	echo The number of files are $FILECOUNT and the number of matching lines are $LINESCOUNT 
+
+# Number of files in the directory and all subdirectories
+FILECOUNT=$(find $1 -type f | wc -l)
+
+# Number of matching lines found in respective files
+LINESCOUNT=$(grep -r $2 $1 | wc -l)
+
+# print message
+echo The number of files are $FILECOUNT and the number of matching lines are $LINESCOUNT 
 
--- finder-app/writer.sh
+++ finder-app/writer.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# Check for error if there are invalid arguments
 if [ ! $# -eq 2 ]
 then 
 	echo Invalid Parameters
@@ -7,8 +8,13 @@ then
 
 fi
 
+# Create the path directory
 mkdir -p $(dirname $1)
+
+# Create new file with name and path
 touch $1 
+
+# Check for error if file is created or not
 if [ -f $1 ]	
 then
 	echo "$2" > $1
