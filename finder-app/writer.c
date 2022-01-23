c53a4539481afa4e550d97cea18b7ab52c804117
DivyeshShashikant <divyesh.patel@colorado.edu>
2022-01-23 15:09:08 -0700

    writer file start

---
+++ finder-app/writer.c
@@ -0,0 +1,62 @@
+#include<stdio.h>
+#include<syslog.h>
+#include<sys/types.h>
+#include<sys/stat.h>
+#include<fcntl.h>
+#include<errno.h>
+#include<unistd.h>
+#include<string.h>
+
+
+int main(int argc, char* argv[])
+{
+	openlog(NULL,0,LOG_USER);
+
+	if(argv[1]==NULL || argv[2]==NULL)
+	{
+		return 1;
+	}
+
+	if(argc!=3)
+	{
+		 syslog(LOG_ERR,"Invalid Number of arguments: %d",argc); 
+		 syslog(LOG_ERR,"1.File Path"); 
+		 syslog(LOG_ERR,"2.String to be entered in the file");	
+ 		 return 1;
+	}
+	
+	int fd, wrbytes, closestat;
+	fd = creat (argv[1], 0777);
+	if(fd == -1)
+	{
+		syslog(LOG_ERR,"File cannot be created: %s",argv[1]);
+		printf("file not created\n");	
+		return -1;	
+	}	
+	else
+	{
+		syslog(LOG_DEBUG,"File created successfully: %s",argv[1]);
+		printf("File created successgfully\n");
+		wrbytes = write(fd, argv[2], strlen(argv[2]));
+		if(wrbytes == -1)
+		{
+			syslog(LOG_ERR,"Write not completed");
+			return -1;
+		}
+		syslog(LOG_DEBUG,"Writing %s to %s",argv[2],argv[1]);
+		printf("Writing to %s to %s\n",argv[2],argv[1]);
+		printf("number of byters written %d\n",wrbytes);
+		closestat = close(fd);
+		if(closestat == -1)
+		{
+			syslog(LOG_ERR,"File not closed");
+			return -1;
+		}
+		printf("%d close status\n",closestat);
+
+	}
+	
+
+	closelog();
+	return 0;
+}
