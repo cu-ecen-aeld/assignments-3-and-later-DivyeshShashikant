befe3b48166817a8524248ec3de52340c109e9e7
DivyeshShashikant <divyesh.patel@colorado.edu>
2022-01-23 21:09:33 -0700

    final code with comments

--- finder-app/writer.c
+++ finder-app/writer.c
@@ -1,3 +1,8 @@
+/*@author: Divyesh  Patel
+ *@filename: writer.c
+ *@brief: ASsignment 2: File Operations and Cross Compiler
+*/
+
 #include<stdio.h>
 #include<syslog.h>
 #include<sys/types.h>
@@ -10,8 +15,10 @@
 
 int main(int argc, char* argv[])
 {
+	//opens a connection to system logger and uses LOG_USER facility
 	openlog(NULL,0,LOG_USER);
 
+	//check is 3 arguments are provided or not
 	if(argc!=3)
 	{
 		 syslog(LOG_ERR,"Invalid Number of arguments: %d",argc); 
@@ -20,6 +27,7 @@ int main(int argc, char* argv[])
  		 return 1;
 	}
 
+	//check if empty string is not passed as filename
 	if(*argv[1]=='\0')
 	{
 		printf("Invalid File name \n");
@@ -28,18 +36,26 @@ int main(int argc, char* argv[])
 	}
 	
 	int fd, wrbytes, closestat;
+
+	//creates a new file or rewrites an existing one
 	fd = creat (argv[1], 0777);
+	
+	//check for errors if file not created
 	if(fd == -1)
 	{
 		syslog(LOG_ERR,"File cannot be created: %s",argv[1]);
-		printf("file not created\n");	
+		printf("File not created\n");	
 		return 1;	
 	}	
 	else
 	{
 		syslog(LOG_DEBUG,"File created successfully: %s",argv[1]);
-		printf("File created successgfully\n");
+		printf("File created successfully\n");
+		
+		//write to file (fd)
 		wrbytes = write(fd, argv[2], strlen(argv[2]));
+		
+		//check if write fail
 		if(wrbytes == -1)
 		{
 			syslog(LOG_ERR,"Write not completed");
@@ -47,18 +63,21 @@ int main(int argc, char* argv[])
 		}
 		syslog(LOG_DEBUG,"Writing %s to %s",argv[2],argv[1]);
 		printf("Writing to %s to %s\n",argv[2],argv[1]);
-		printf("number of byters written %d\n",wrbytes);
+	
+		//close fd
 		closestat = close(fd);
+
+		//check for errors if file failed to close
 		if(closestat == -1)
 		{
 			syslog(LOG_ERR,"File not closed");
 			return 1;
 		}
-		printf("%d close status\n",closestat);
+		printf("close status %d\n",closestat);
 
 	}
 	
-
+	//close log
 	closelog();
 	return 0;
 }
